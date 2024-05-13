12d5fd669926063cf435bc383a2d8dfc69623b72
RecFanJia <geo.fanjia@gmail.com>
2024-05-13 14:02:23 +0200

    finished 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,15 @@
 const dummy = (array) => {
     return 1
   }
-  
-module.exports = {dummy}
+
+const totalLikes = (array) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+
+    return array.length === 0
+    ? 0
+    : array.reduce(reducer,0)
+ }
+
+module.exports = {dummy, totalLikes}
