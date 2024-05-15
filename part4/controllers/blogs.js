124cf88d2795dc277ff132b599e0e533aa37e516
RecFanJia <geo.fanjia@gmail.com>
2024-05-15 20:46:56 +0200

    finished 4.21*

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -58,52 +58,33 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 });
 
-// 更新博客
-blogsRouter.put('/:id', async (req, res, next) => {
-  const { title, author, url, likes } = req.body;
-  const userId = req.user._id;
-
-  const updatedBlogData = {
-    title,
-    author,
-    url,
-    likes,
-    user: userId
-  };
-
-  try {
-    const updatedBlog = await Blog.findByIdAndUpdate(
-      req.params.id,
-      updatedBlogData,
-      { new: true, runValidators: true, context: 'query' }
-    ).populate('user', { username: 1, name: 1 });
-    if (updatedBlog) {
-      res.json(updatedBlog);
-    } else {
-      res.status(404).end();
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
 // 删除博客
 blogsRouter.delete('/:id', async (req, res, next) => {
-  const userId = req.user._id;
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(req.token, process.env.SECRET);
+  } catch (error) {
+    return res.status(401).json({ error: 'token invalid or expired' });
+  }
+
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'token invalid' });
+  }
 
   try {
     const blog = await Blog.findById(req.params.id);
+
     if (!blog) {
       return res.status(404).json({ error: 'Blog not found' });
     }
 
-    if (blog.user.toString() !== userId.toString()) {
+    if (blog.user.toString() !== decodedToken.id.toString()) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
     await Blog.findByIdAndDelete(req.params.id);
 
-    const user = await User.findById(userId);
+    const user = await User.findById(decodedToken.id);
     user.blogs = user.blogs.filter(blogId => blogId.toString() !== req.params.id);
     await user.save();
 
