188b69caf281c795e127fbc258a7ea23e36b1b6f
RecFanJia <geo.fanjia@gmail.com>
2024-05-21 15:11:12 +0200

    finished 5.18

--- part5/cypress/e2e/blog_spec.cy.js
+++ part5/cypress/e2e/blog_spec.cy.js
@@ -1,6 +1,16 @@
 describe('blog app', function() {
     beforeEach(function() {
       cy.request('POST', 'http://localhost:3001/api/testing/reset')
+
+      // Create a new user
+      const user = {
+        name: 'FanJia',
+        username: 'RecFanJia',
+        password: '2301335'
+      }
+      cy.request('POST', 'http://localhost:3001/api/users/', user)
+  
+      // Visit the application
       cy.visit('http://localhost:5173')
     })
     it('Login form is shown', function() {
@@ -9,6 +19,40 @@ describe('blog app', function() {
       cy.get('input[name="Username"]').should('be.visible')
       cy.get('input[name="Password"]').should('be.visible')
       cy.get('#login-button').should('be.visible')
+    })
+
+    describe('Login',function() {
+      it('succeeds with correct credentials', function() {
+        cy.contains('login').click()
+        cy.get('input[name="Username"]').type('RecFanJia')
+        cy.get('input[name="Password"]').type('2301335')
+        cy.get('#login-button').click()
+        // Verify correct login
+        cy.get('.notification')
+        .should('be.visible')
+        .and('contain', 'Welcome FanJia')
+        cy.contains('FanJia logged-in').should('be.visible')
+
+        // Verify that the notification is green
+        cy.get('.notification')
+        .should('have.css', 'color', 'rgb(0, 128, 0)')
+        
+      })
+  
+      it('fails with wrong credentials', function() {
+        cy.contains('login').click()
+        cy.get('input[name="Username"]').type('RecFanJia')
+        cy.get('input[name="Password"]').type('wrongnumber')
+        cy.get('#login-button').click()
+        // Verify failed login
+        cy.get('.error')
+          .should('be.visible')
+          .and('contain', 'Wrong username or password')
+
+        // Verify that the notification is red
+        cy.get('.error')
+          .should('have.css', 'color', 'rgb(255, 0, 0)')
+      })
     })
   })
   //beforeEach(function() {
