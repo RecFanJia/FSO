7d66964b7a327763b947ed79e63100bfdf1a89f8
RecFanJia <geo.fanjia@gmail.com>
2024-05-27 18:34:25 +0200

    finished 6.19

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -10,7 +10,7 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     dispatch(addAnecdote(content))
-    dispatch(showNotification(`You added "${content}"`, 5))
+    dispatch(showNotification(`new anecdote "${content}"`, 5))
   }
 
   return (
